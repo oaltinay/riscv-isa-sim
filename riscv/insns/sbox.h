1c40677d34db6f2f9d5162e4d960f9d66a32b47b
Ozlem Altinay <oaltinay@github.com>
2021-04-23 15:49:40 +0300

    convert signed int to unsigned int

--- riscv/insns/sbox.h
+++ riscv/insns/sbox.h
@@ -1,13 +1,13 @@
 //Load State vector
-reg_t x0 = MMU.load_int32(RS1);
-reg_t x1 = MMU.load_int32(RS1+4);
-reg_t x2 = MMU.load_int32(RS1+8);
-reg_t x3 = MMU.load_int32(RS1+12);
-reg_t x4 = MMU.load_int32(RS1+16);
+reg_t x0 = MMU.load_uint32(RS1);
+reg_t x1 = MMU.load_uint32(RS1+4);
+reg_t x2 = MMU.load_uint32(RS1+8);
+reg_t x3 = MMU.load_uint32(RS1+12);
+reg_t x4 = MMU.load_uint32(RS1+16);
 
 reg_t t0, t1, t2, t3, t4;
 
-//S-Box
+//S-Box 
 x0 ^= x4;
 x4 ^= x3;
 x2 ^= x1;
@@ -25,10 +25,11 @@ x1 ^= x0;
 x3 ^= x2;
 x0 ^= x4;
 
-//Load State Vector
+//Store State Vector
 MMU.store_uint32(RS1, x0);
 MMU.store_uint32(RS1+4, x1);
 MMU.store_uint32(RS1+8, x2);
 MMU.store_uint32(RS1+12, x3);
 MMU.store_uint32(RS1+16, x4);
 WRITE_RD(x0);
+
--- riscv/insns/sboxi.h
+++ riscv/insns/sboxi.h
@@ -1,12 +1,10 @@
-//Load State vector
-reg_t x0 = MMU.load_int32(RS1);
-reg_t x1 = MMU.load_int32(RS1+4);
-reg_t x2 = MMU.load_int32(RS1+8);
-reg_t x3 = MMU.load_int32(RS1+12);
-reg_t x4 = MMU.load_int32(RS1+16);
-
+//Load State array
+reg_t x0 = MMU.load_uint32(RS1);
+reg_t x1 = MMU.load_uint32(RS1+4);
+reg_t x2 = MMU.load_uint32(RS1+8);
+reg_t x3 = MMU.load_uint32(RS1+12);
+reg_t x4 = MMU.load_uint32(RS1+16);
 reg_t t0, t1, t2, t3, t4;
-
 //S-Box
 x0 ^= x4;
 x4 ^= x3;
@@ -24,11 +22,10 @@ x3 = t3 ^ (~t4 & t0);
 x1 ^= x0;
 x3 ^= x2;
 x0 ^= x4;
-
-//Load State Vector
-MMU.store_uint32(RS1, x0);
+//Store State Array
+MMU.store_uint32(RS1,   x0);
 MMU.store_uint32(RS1+4, x1);
 MMU.store_uint32(RS1+8, x2);
-MMU.store_uint32(RS1+12, x3);
-MMU.store_uint32(RS1+16, x4);
+MMU.store_uint32(RS1+12,x3);
+MMU.store_uint32(RS1+16,x4);
 WRITE_RD(x0);
